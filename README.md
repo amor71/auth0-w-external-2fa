76744e34180f57baae3e83f776a2ea99af45aed2
Amichay Oren <amichay@sgeltd.com>
2022-08-21 13:32:28 +0300

    implement twilio 2FA flow

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Proof of Concept for integrating AUTH0 with an external 2FA.
 * Follow setups to create email verification via [SendGrid](https://docs.sendgrid.com/for-developers/sending-email/api-getting-started). SendGrid is a Twilio company for email sending,
 * Note that both services provide a `freemium` tier for development.
 * In SendGrid - create a validated "sender", and a [template](https://www.twilio.com/docs/verify/email#find-template-id),
-* In Twilio - create the Verify service, by email and add email integration to SendGrid. 
+* In Twilio - create the Verify service, by email and add email integration to SendGrid.
 
 **Configuration**
 
@@ -23,14 +23,22 @@ Proof of Concept for integrating AUTH0 with an external 2FA.
 ```python
     domain = ""
 
+    # Auth0
     client_id = ""
     client_secret = ""
     
     password = ""
     username = ""
 
+    # SendGrid
     sender = ""
     sendgrid_api_key = ""
+
+    # Twilio
+    twilio_account_sid = ""
+    twilio_auth_token = ""
+    twilio_verify_service_id = ""
+
 ```
 
 **
--- tests/test_twilio_email_2fa.py
+++ tests/test_twilio_email_2fa.py
@@ -1,5 +1,43 @@
 import toml
+from twilio.rest import Client
+
+secrets = toml.load(".creds.toml")
 
 
-def test_email_2fa():
-    toml.load(".creds.toml")
+def test_twilio_auth():
+    Client(secrets["twilio_account_sid"], secrets["twilio_auth_token"])
+
+    return True
+
+
+def test_twilio_email():
+    client = Client(
+        secrets["twilio_account_sid"], secrets["twilio_auth_token"]
+    )
+
+    verification = client.verify.v2.services(
+        secrets["twilio_verify_service_id"]
+    ).verifications.create(to="amichay.oren@gmail.com", channel="email")
+
+    print(verification.sid)
+
+
+def test_twilio_email_verify():
+    client = Client(
+        secrets["twilio_account_sid"], secrets["twilio_auth_token"]
+    )
+
+    print("Enter verification code: ")
+    verification_code = input()
+
+    verification_check = client.verify.v2.services(
+        secrets["twilio_verify_service_id"]
+    ).verification_checks.create(
+        to="amichay.oren@gmail.com", code=verification_code
+    )
+
+    if verification_check.status != "approved":
+        print(f"Verification failed w {verification_check.status}")
+        return False
+
+    return True
